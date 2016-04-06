990d2630d3ccb72a1c902343607e84ff5af9a800
Fred Klaus <development@fkweb.de>
2016-04-07 00:21:11 +0200

    Change build files

--- cmake.sh
+++ cmake.sh
@@ -6,10 +6,22 @@ mkdir -p build
 rm -R bin/*
 rm -R build/*
 
+
 cmake -H. -Bbuild -G"Kate - Unix Makefiles"
+
 
 pushd build > /dev/null
 
-cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr/local ../
+if [ ! $# == 1 ]; then
+    set $1 Release
+fi
+
+cmake -DCMAKE_BUILD_TYPE=$1 -DCMAKE_INSTALL_PREFIX=/usr/local ../
+#make edit_cache
+
+
+echo
+echo "CMAKE_BUILD_TYPE     = $1"
+echo "CMAKE_INSTALL_PREFIX = /usr/local"
 
 popd >/dev/null
