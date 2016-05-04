dbd50769952bbb7bf88694c0e40cb513c3524020
Fred Klaus <development@fkweb.de>
2016-05-04 18:47:57 +0200

    Change comment header

--- src/sdl/audio.cpp
+++ src/sdl/audio.cpp
@@ -1,6 +1,6 @@
 /***************************************************************************
- *   Copyright (C) 2005-2013 by Fred Klaus <development@fkweb.de>          *
- *                                                                         *
+ *   Copyright (C) by Fred Klaus                                           *
+ *       development@fkweb.de                                              *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
